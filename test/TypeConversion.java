48e82594f667d2168616862cb0a0c951990755c3
José Luis Sampedro <jsampedro@protonmail.com>
2021-03-23 20:55:37 +0000

    Conversion of Type int to char

--- test/TypeConversion.java
+++ test/TypeConversion.java
@@ -1,16 +1,19 @@
 /**
- * Example array with included type conversion.
+ * Example of type conversion.
  */
-public class ArraySimple {
+
+public class TypeConversion {
     public static void main(String[] args) {
-        // Define an empty array of ten elements. 
+
+        // Define an empty array of n elements. 
         int sample[] = new int[10];
+        
         // Define variable i as zero.
         int i = 0;
         // Define the variable ch as the letter A.
         char ch = 'A';
         
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < sample.length; j++) {
             // The element i of the matrix takes the value of the letter in ch, A.
             sample[i] = ch;
                 //Convert i to char to show the letter.    
