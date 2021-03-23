c45c3432e0d090c9ebeaf1373715176d58bc5a4d
José Luis Sampedro <jsampedro@protonmail.com>
2021-03-23 19:51:37 +0000

    Simple Show Date class

---
+++ src/ShowDate.java
@@ -0,0 +1,16 @@
+/**
+ * Simple show date class.
+ * Import only de class Date.
+ */
+
+import java.util.Date;
+
+public class ShowDate {
+
+    public static void main(String[] args) {
+        
+        System.out.println(new Date());
+        
+    }
+
+}
