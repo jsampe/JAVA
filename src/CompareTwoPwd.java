4951aa8ab1901a2dbd69c0e0b857e273d77609ab
José Luis Sampedro <jsampedro@protonmail.com>
2021-03-24 18:16:01 +0000

    Compare two strings

--- src/CompareTwoPwd.java
+++ src/CompareTwoPwd.java
@@ -1,16 +1,23 @@
+/**
+ * Shows how two text strings can be compared.
+ * Shows case sensitive and normal use:
+ * .equals() is case sensitive.
+ * .equalsIgnoreCase ignore upper case and lower case.
+ */
+
 public class CompareTwoPwd {
 
     public static void main(String[] args) {
-        // -----------------------------------------
-        //Compare two passwords.
-
-        //String password_1, password_2;
-
-        //password_1 = "Bilbo";
-        //password_2 = "bilbo";
-
-        //System.out.println(password_1.equals(password_2));
-        //System.out.println(password_1.equalsIgnoreCase(password_2));
+        
+        // Define two strings.
+        String password_1, password_2;
+        // Assign values upper case and lower case.
+        password_1 = "BILBO";
+        password_2 = "bilbo";
+        // Compare and show false, are case sensitive.
+        System.out.println(password_1.equals(password_2));
+        // Compare and show true, ignore upper and lower case.
+        System.out.println(password_1.equalsIgnoreCase(password_2));
     }
     
 }
