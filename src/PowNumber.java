5ff59a681d9c31a7e2097c0ef21808501e218ae5
José Luis Sampedro <jsampedro@protonmail.com>
2021-03-24 19:00:54 +0000

    Show the use of Pow( ) with type conversion

--- src/PowNumber.java
+++ src/PowNumber.java
@@ -1,16 +1,20 @@
-/* Returns the value of the first argument raised
- to the power of the second argument*/
+/** 
+ * Returns the value of the first argument raised to the power of the second argument.
+ */
 
 public class PowNumber {
 
     public static void main(String[] args) {
-      
+    
+        // Define two variables, as the base and exponent of a pow.
         int base;
         int exp;
 
+        // Asign values, five to the power of three.
         base = 5;
         exp  = 3;
 
+        // Use pow() that returns a double, and convert to int.
         int result = (int) Math.pow(base, exp);
 
         System.out.println(
