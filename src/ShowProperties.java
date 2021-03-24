c1e7a19d265a76fe1d2f80114c311861671da596
José Luis Sampedro <jsampedro@protonmail.com>
2021-03-24 20:12:15 +0000

    How to show Java properties

--- src/ShowProperties.java
+++ src/ShowProperties.java
@@ -1,7 +1,15 @@
+/**
+ * Show the use of ShowProperties.
+ */
+
 public class ShowProperties {
     public static void main(String[] args) {
-        //System.getProperties().list(System.out);
+        
+        // Show all the properties.
+        System.getProperties().list(System.out);
+        // Show only the user name.
         System.out.println(System.getProperty("user.name"));
+        // Show only java version.
         System.out.print(System.getProperty("java.runtime.version"));        
     }
 }
