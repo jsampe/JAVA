4450e15a93c22933c9eb1fb9b82a15a0f1972b33
jsampe <jsampedro@protonmail.com>
2021-04-15 18:31:01 +0100

    How to order array

--- test/Bubble.java
+++ test/Bubble.java
@@ -2,35 +2,27 @@ class Bubble {
 
     public static void main(String[] args) {
         
-        int nums[] = {99, -10, 100, 18, -978, 562, 463, 9, 287, 49};
+        int numbers[] = {99, -10, 100, 18, -978, 562, 463, 9, 287, 49};
 
-        int a, b, t;
-        //int size;
+        int p;    // Se refiere a la posición dentro de la matriz.
+        int temp; // Se usa para almacenar temporalmente el valor mayor.
+       
+        // Algoritmo, rucuerde que: Las comparaciones ">" se hacen se derecha a izquierda, y las asignaciones = de izquierda a derecha.
 
-        //size = nums.length; // Number of elements to order.
+        for (int i=1; i < numbers.length; ++i) { // Recorre la matriz de 
 
-        // Shows original array.
-        for ( int i = 0; i < nums.length; i++ ) {
-            System.out.printf( "%d \t", nums[i] );
-        }
-
-        System.out.println("\n Now ordered:");
-
-        // Bubble order.
-        for ( a=1; a < nums.length; ++a) {
-            for ( b=nums.length-1; b >= a; b-- ) {
-                if (nums[b-1] > nums[b]) { // If no are in order, change elements.
-                    t = nums[b-1];
-                    nums[b-1] = nums[b];
-                    nums[b] = t;
+            for ( p = numbers.length-1; p >= i; p-- ) {
+                if (numbers[p-1] > numbers[p]) { // Empezando por el final, y de dos en dos. Comprueba si el valor de la izquierda es mayor que la de la derecha.
+                    temp = numbers[p-1]; // Si el de la izquierda es mayor se almacena en la variable temp para no perderlo.
+                    numbers[p-1] = numbers[p]; // Se copia el valor menor de la derecha hacia la izquierda. En este punto los dos valores son iguales.
+                    numbers[p] = temp; // Ahora copia en la posición de la derecha, el valor mayor, almacenado en temp. Ahora el menor esta a la izquierda.
                 }
             }
+
         }
 
-        for ( int i = 0; i < nums.length; i++ ) {
-            System.out.printf( "%d \t", nums[i] );
+        for ( int i = 0; i < numbers.length; i++ ) {
+            System.out.printf( "%d \t", numbers[i] ); // Print ordered array.
         }
-
     }
-    
 }
