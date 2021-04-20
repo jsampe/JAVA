1ffbb711a972a02c075055883e8c3db1b5a36573
José Luis Sampedro <jsampedro@protonmail.com>
2021-04-20 21:02:23 +0100

    Test

--- test/test.java
+++ test/test.java
@@ -1,7 +1,20 @@
-public class test {
-    public static void main(String[] args) {
+class test {
 
-        System.out.println(" ");
+    public static void main(String[] args) {
         
+        int table[][] = new int[4][4];
+        char ch = 'A';
+        char ct = 'E';
+
+        for (int i = 0; i < table.length; i++) {
+            
+
+            
+            table[i] = ch;
+            System.out.println((char)table[i]);
+            ++ch;
+        }        
+
     }
+    
 }
