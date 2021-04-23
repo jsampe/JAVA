ec72a11b63b892f34304aa14f2bebe6313ecb88d
José Luis Sampedro <jsampedro@protonmail.com>
2021-04-23 19:51:59 +0100

    Test

--- test/test.java
+++ test/test.java
@@ -1,6 +1,16 @@
+import java.util.Arrays;
+
 public class test {
 
-
-
-
+    public static void main(String[] args) {
+        
+        String employed[][] = {
+            {"Jhon" , "Mary" , "David" , "Jessica"},
+            {"Onwer" , "Arquited" , "Enginered" , "Teacher"}
+        };
+        
+        for
+        
+    }
+    
 }
