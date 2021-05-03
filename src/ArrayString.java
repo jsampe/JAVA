27ba2c2308d9de537d100326c097d5c4a0b386b5
José Luis Sampedro <jsampedro@protonmail.com>
2021-05-03 19:53:16 +0100

    How to arrays works with strings

--- src/ArrayString.java
+++ src/ArrayString.java
@@ -1,6 +1,6 @@
 import java.util.Arrays;
 
-public class test {
+public class ArrayString {
 
     public static void main(String[] args) {
         
@@ -12,7 +12,11 @@ public class test {
         for (String[] strings : employed) {
             System.out.println(Arrays.toString(strings));
         }
-        
+        System.out.println(employed[1][1]);
+
+        System.out.println(employed.length);
+        System.out.println(employed[0].length);
+        System.out.println(employed[1].length);
     }
     
 }
