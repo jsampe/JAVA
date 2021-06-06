d25492aefe2729c3ec683e5adef56e807a732eb3
jsampe <jsampedro@protonmail.com>
2021-06-06 18:26:40 +0100

    Test added

---
+++ test/test.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+public class test {
+
+    public static void main(String[] args) {
+        
+        String employed[][] = {
+            {"Jhon" , "Mary" , "David" , "Jessica"},
+            {"Onwer" , "Arquited" , "Enginered" , "Teacher"}
+        };
+        
+        for (String[] strings : employed) {
+            System.out.println(Arrays.toString(strings));
+            System.out.println("Hello");
+            
+        }
+        
+    }
+    
+}
